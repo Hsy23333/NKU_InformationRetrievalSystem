074e004efa3d7d1897ff333d84ca9eaf76e4998d
黄尚扬 <14164494+hsy23333@user.noreply.gitee.com>
2025-05-13 17:20:22 +0800

    12club爬取成功

---
+++ spider/tutorial/tutorial/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NkuspiderItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+    page_links = scrapy.Field()
+    crawl_time = scrapy.Field()
